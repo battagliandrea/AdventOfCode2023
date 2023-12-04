5e24b3b62acfab71c68f5d8b17450618b82a5c53
Andrea Battaglia <battaglia.andry@gmail.com>
2023-12-04 17:01:43 +0100

    feat: add day template

---
+++ src/main/kotlin/it/battagliandrea/advent/of/code/DayXTemplate.kt
@@ -0,0 +1,19 @@
+package it.battagliandrea.advent.of.code
+
+/*
+
+*/
+
+fun main(args: Array<String>) {
+    println("Day X")
+
+    part1()
+    part2()
+}
+
+private fun part1(){
+
+}
+
+private fun part2(){
+}
